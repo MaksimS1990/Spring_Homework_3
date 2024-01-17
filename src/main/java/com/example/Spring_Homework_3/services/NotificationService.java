c78f226997f25ba413bfcbc12c2dc2948d898027
MaksimS <sipatovGT@mail.ru>
2024-01-17 15:56:29 +0400

    create_project_HW3

---
+++ src/main/java/com/example/Spring_Homework_3/services/NotificationService.java
@@ -0,0 +1,16 @@
+package com.example.Spring_Homework_3.services;
+
+import com.example.Spring_Homework_3.domain.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    public void notifyUser(User user) {
+        System.out.println("A new user has been created: " + user.getName());
+    }
+
+    public void sendNotification(String s) {
+        System.out.println(s);
+    }
+}
---
+++ src/test/java/com/example/Spring_Homework_3/SpringHomework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Homework_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomework3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
